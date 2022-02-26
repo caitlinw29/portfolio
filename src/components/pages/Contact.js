57cd6e153fc96fb8fcb877692130860181244993
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-25 19:46:37 -0500

    Add contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,8 +5,23 @@ export default function Contact() {
     <div className='px-5 py-4'>
       <h1>Contact Me</h1>
       <p>
-        This will be the contact page
+        Email: caitlinwall96@gmail.com
       </p>
+      <form>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="name" class="form-control" id="name" placeholder="Name" />
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" class="form-control" id="email" placeholder="Email" />
+        </div>
+        <div class="form-group">
+          <label class="form-check-label" for="message">Message:</label>
+          <input type="textArea" class="form-control" id="message" placeholder='Message' />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 }
