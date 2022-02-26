6ffd903d5836669da48ddcd851e01d06b3168354
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-25 19:40:26 -0500

    Add about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,15 @@ export default function About() {
   return (
     <div className='px-5 py-4'>
       <h1>About Me</h1>
-      <p>
-       This will be the about page
-      </p>
+      <div className='d-flex justify-content-center'>
+        <img src={process.env.PUBLIC_URL + '/images/me.jpg'} alt="Caitlin" />
+
+        <p className='px-4'>
+        Hi! I'm Caitlin, a teacher turned techie! I have experience in the MERN stack and love to explore new technologies.
+        </p>
+        
+       
+      </div> 
     </div>
   );
 }
