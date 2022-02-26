25920fa5d3db984e6c4e8f2b90df54c39da7b5e6
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-25 20:00:49 -0500

    Add resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,9 +4,28 @@ export default function Resume() {
   return (
     <div className='px-5 py-4'>
       <h1>Resume</h1>
-      <p>
-        This will be the resume page
-      </p>
+      <a href={process.env.PUBLIC_URL + '/CaitlinWall_Resume.pdf'} target="_blank" rel='noreferrer'>View my resume here</a>
+      <p>Front-end Proficiencies</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+        <li>Bootstrap/Materialize</li>
+      </ul>
+      <p>Back-end Proficiencies</p>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>mySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
+
     </div>
   );
 }
